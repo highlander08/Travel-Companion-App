1e24e5b86af5bc72d94365b8d42c54b18de4ad90
highlander santos <highlander@lanlink.com.br>
2022-10-31 16:29:40 -0300

    complete

---
+++ src/api/travelAdvisorAPI.js
@@ -0,0 +1,62 @@
+/* eslint-disable consistent-return */
+import axios from "axios";
+
+// const options = {
+//   params: {
+//     bl_latitude: "11.847676",
+//     tr_latitude: "12.838442",
+//     bl_longitude: "109.095887",
+//     tr_longitude: "109.149359",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": "42280744bemshdaeac3af2aa729fp183861jsnf79f9aae14bb",
+//     "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+//   },
+// };
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          "x-rapidapi-key": process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat, lon: lng },
+          headers: {
+            "x-rapidapi-key": process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
